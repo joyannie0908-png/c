c35be1779f4e4e0481f6e75b645e68a0a9813326
git config --global user.name "joyannie0908 <git config --global user.email "joyannie0908@gmail.com>
2026-09-18 08:22:43 +0900

    2주차실습1

--- Project1/week2.cpp
+++ Project1/week2.cpp
@@ -2,8 +2,11 @@
 
 int main(void)
 {
-	float num = 82.125;
-	double dnum = 82.125;
+	int val = 21;
+	int val_minus = -21;
+	int result = 0;
 
+	result = val + val_minus;
+	printf("result is % d\n", result);
 	return 0;
 }
